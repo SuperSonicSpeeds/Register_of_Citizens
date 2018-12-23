f8be7d5b29a7f1f5c8dcf515b7305cf97b75d5bc
Seedsiz <46053902+Seedsiz@users.noreply.github.com>
2018-12-23 23:39:50 +0100

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class Person {
-    String name;
-    String fandom;
+    private String name;
+    private String fandom;
     private boolean active = false;
     private int arrival;
+    private String type;
 
 
-    public Person(String n, String f) {
+    public Person(String n, String f, String t) {
         this.name = n;
         this.fandom = f;
+        this.type = t;
     }
     
     public String getName() {
@@ -19,4 +21,21 @@ public class Person {
     public String getFandom() {
         return this.fandom;
     }
+    
+    public String getType() {
+        return this.type;
+    }
+    
+    public void setStatus() {
+        this.active = true;
+    }
+    
+    public boolean getStatus() {
+        return this.active;
+    }
+    
+    public String getInformation() {
+        return this.name + " is a " + this.type + " character from " + this.fandom + ".";
+    }
+    
 }
